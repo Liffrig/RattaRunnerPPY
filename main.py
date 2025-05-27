ffa11b196a26f90cf29ffe8974c2aa7da4490d8c
Liffrig <liffrig@duck.com>
2025-05-27 18:26:12 +0200

    +wall building logic

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ from __future__ import annotations
 import itertools
 
 from model.labyrinth import Labyrinth
+from utils.random_engine import labyrinth_architect
 
 
 def main() -> None:
-    l = Labyrinth(3,3)
-    print(l.size)
-
-    
-
-
-
+    print("start")
+    g = labyrinth_architect(7)
+    g.send(None)
+    print(g.send([5,6]))
+    print(g.send([1, 2]))
 
 
 if __name__ == "__main__":
--- utils/random_engine.py
+++ utils/random_engine.py
@@ -1,8 +1,15 @@
-import random
+from typing import List, Generator
+from random import choice
 
+def labyrinth_architect(free_properties: List[int] | int) -> Generator[int, None, None]:
+	where_to_build = None
+	possible_positions = None
+	while True:
+		if type(free_properties) == int:
+			possible_positions = [x for x in range(free_properties)]
+			where_to_build = yield choice(possible_positions)
+		else:
+			possible_positions  -= free_properties
+			where_to_build = yield choice(possible_positions)
 
-class RandomEngine:
-
-
-	def __init__(self):
-		raise NotImplementedError
+		print(possible_positions)
